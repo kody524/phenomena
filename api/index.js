368f8fcece0e8a228c0449bc46de7af5ac7193a9
kody524 <kodyrichardson@kodys-mbp.lan>
2023-01-25 20:19:59 -0600

    completed

--- api/index.js
+++ api/index.js
@@ -1,5 +1,7 @@
 // Build an apiRouter using express Router
-
+const express = require('express');
+const apiRouter = express.Router();
+const{ getOpenReports,createReport,_getReport,closeReport,createReportComment} = require('../db')
 
 // Import the database adapter functions from the db
 
@@ -12,7 +14,19 @@
  * - on success, it should send back an object like { reports: theReports }
  * - on caught error, call next(error)
  */
+apiRouter.get('/reports',async (req,res,next)=>{
+    try{
+const reports = await getOpenReports();
+console.log(reports)
+res.send({
+    reports:reports
+})
+    }catch(error){
+        next(error)
+    }
+   
 
+})
 
 
 /**
@@ -23,7 +37,17 @@
  * - on success, it should send back the object returned by createReport
  * - on caught error, call next(error)
  */
-
+apiRouter.post('/reports',async(req,res,next)=>{
+    try{
+        const data = await createReport(req.body);
+        res.send(
+            data
+        )
+    }catch(error){
+        next(error)
+    }
+    
+})
 
 
 /**
@@ -35,7 +59,17 @@
  * - on success, it should send back the object returned by closeReport
  * - on caught error, call next(error)
  */
-
+apiRouter.delete('/reports/:reportId',async(req,res,next)=>{
+    try{
+        const data = await closeReport(req.params.reportId,req.body.password)
+        
+        res.send(
+            data
+        )
+    }catch(error){
+        next(error)
+    }
+})
 
 
 /**
@@ -47,7 +81,17 @@
  * - on success, it should send back the object returned by createReportComment
  * - on caught error, call next(error)
  */
+apiRouter.post('/reports/:reportId/comments',async(req,res,next)=>{
+    try{
+        const data = await createReportComment(req.params.reportId,req.body)
+        res.send(data)
+    }catch(error){
+        next(error)
+    }
+    
 
+})
 
 
 // Export the apiRouter
+module.exports = {apiRouter}
